7b1661f60de7662077c4832467f97fb3c12cf238
evgenymatveev <evgeni2007@yandex.ru>
2017-12-11 13:42:25 +0300

    remove .idea

--- 1.JavaSyntax/src/com/javarush/task/task03/task0303/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0303/Solution.java
@@ -6,10 +6,11 @@ package com.javarush.task.task03.task0303;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        System.out.println(convertEurToUsd(57, 1.5));
+        System.out.println(convertEurToUsd(89, 1.2));
     }
 
     public static double convertEurToUsd(int eur, double course) {
-        //напишите тут ваш код
+        return eur * course;
     }
 }
