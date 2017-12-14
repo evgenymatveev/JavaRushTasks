074afd579ac2251b4954ce54b61e44ad6a5bb141
evgenymatveev <evgeni2007@yandex.ru>
2017-12-14 20:28:27 +0300

    level 3 task 14

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0314/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task03.task0314;
+
+/* 
+Таблица умножения
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[i] * arr[j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
