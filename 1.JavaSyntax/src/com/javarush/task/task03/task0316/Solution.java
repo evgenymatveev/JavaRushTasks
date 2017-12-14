f457d3a0e45cb3c6e623d0e29723dae14987e773
evgenymatveev <evgeni2007@yandex.ru>
2017-12-14 20:28:55 +0300

    level 3 task 16

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0316/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task03.task0316;
+
+/* 
+Экранирование символов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //It's Windows path: "C:\Program Files\Java\jdk1.7.0\bin"
+        //It's Java string: \"C:\\Program Files\\Java\\jdk1.7.0\\bin\"
+        System.out.println("It" + "\'" + "s Windows path: " + '\"' + "C:" + "\\" + "Program Files"
+                + "\\" + "Java" + "\\" + "jdk1.7.0" + "\\" + "bin" + '\"');
+        System.out.println("It" + "\'" + "s Java string: " + "\\" + '\"' + "C:" + "\\" + "\\"
+                + "Program Files" + "\\" + "\\" + "Java" + "\\" + "\\"
+                + "jdk1.7.0" + "\\" + "\\" + "bin" + "\\" + '\"');
+    }
+}
