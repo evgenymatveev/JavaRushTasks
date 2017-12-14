8a6ef02bdeaa19507c1fd41d761d2cdee5dda9e3
evgenymatveev <evgeni2007@yandex.ru>
2017-12-14 20:49:16 +0300

    level 3 task 18

--- 1.JavaSyntax/src/com/javarush/task/task03/task0318/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0318/Solution.java
@@ -10,9 +10,9 @@ import java.io.InputStreamReader;
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String name = bufferedReader.readLine();
         String number = bufferedReader.readLine();
+        String name = bufferedReader.readLine();
         int n = Integer.parseInt(number);
-        System.out.println(name + " " + "захватит мир через " + n + " лет. Му-ха-ха!");
+        System.out.println(name + " захватит мир через " + n + " лет. Му-ха-ха!");
     }
 }
