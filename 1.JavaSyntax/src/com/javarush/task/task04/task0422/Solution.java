2dfaa4560d2adad825ed9b5189a4c562250e3e0c
evgenymatveev <evgeni2007@yandex.ru>
2018-01-02 14:25:58 +0300

    level 4 task 22

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0422/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task04.task0422;
+
+/* 
+18+
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        bufferedReader.readLine();
+        int age = Integer.parseInt(bufferedReader.readLine());
+        if (age < 18) {
+            System.out.println("Подрасти еще");
+        }
+    }
+}
