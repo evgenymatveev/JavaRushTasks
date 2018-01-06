e0d935e754051aa9a7dc94109a4ea06b8ae56676
evgenymatveev <evgeni2007@yandex.ru>
2018-01-06 16:45:33 +0100

    level 4 task 30

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0430/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task04.task0430;
+
+/* 
+От 1 до 10
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        int n = 1;
+        while (n < 11) {
+            System.out.println(n);
+            n++;
+        }
+
+    }
+}
