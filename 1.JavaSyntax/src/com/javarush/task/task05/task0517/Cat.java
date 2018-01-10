0dfad3e9be40d8b8dea83ec48c44666264b208c0
evgenymatveev <evgeni2007@yandex.ru>
2018-01-10 20:01:29 +0300

    level 5 task 17

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0517/Cat.java
@@ -0,0 +1,52 @@
+package com.javarush.task.task05.task0517;
+
+/* 
+Конструируем котиков
+*/
+
+public class Cat {
+    private String name;
+    private int age;
+    private int weight;
+    private String address;
+    private String color;
+
+    public Cat(String name) {
+        this.name = name;
+        this.age = 3;
+        this.weight = 5;
+        this.color = "black";
+
+    }
+
+    public Cat(String name, int weight, int age) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.color = "black";
+    }
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.weight = 5;
+        this.color = "black";
+    }
+
+    public Cat(int weight, String color) {
+        this.weight = weight;
+        this.color = color;
+        this.age = 3;
+    }
+
+    public Cat(int weight, String color, String address) {
+        this.weight = weight;
+        this.address = address;
+        this.color = color;
+        this.age = 3;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
