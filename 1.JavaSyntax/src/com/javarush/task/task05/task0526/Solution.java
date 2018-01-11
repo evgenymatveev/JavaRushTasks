a0334331a633125f635da4cf86f2e7a5c68219f2
evgenymatveev <evgeni2007@yandex.ru>
2018-01-11 23:01:16 +0300

    level 3 task 22

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0526/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task05.task0526;
+
+/* 
+Мужчина и женщина
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Man man1 = new Man("Evgen", 40, "123");
+        Man man2 = new Man("Slava", 30, "456");
+        Woman woman1 = new Woman("Sveta", 20, "123456");
+        Woman woman2 = new Woman("Tanya", 25, "123456");
+        System.out.println(man1.name + " " + man1.age + " " + man1.address);
+        System.out.println(man2.name + " " + man2.age + " " + man2.address);
+        System.out.println(woman1.name + " " + woman1.age + " " + woman1.address);
+        System.out.println(woman2.name + " " + woman2.age + " " + woman2.address);
+    }
+
+    public static class Man {
+        private String name;
+        private int age;
+        private String address;
+
+        public Man(String name, int age, String address) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+    }
+
+    public static class Woman {
+        private String name;
+        private int age;
+        private String address;
+
+        public Woman(String name, int age, String address) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+    }
+}
+
