2d7f915315b5243d8645a2ce93673934110ef11c
evgenymatveev <evgeni2007@yandex.ru>
2018-01-15 20:09:23 +0300

    level 6 task 06

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0606/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task06.task0606;
+
+import java.io.*;
+
+/* 
+Чётные и нечётные циферки
+*/
+
+public class Solution {
+
+    public static int even;
+    public static int odd;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        String s = Integer.toString(n);
+        char[] ch = s.toCharArray();
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.getNumericValue(ch[i]) % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+        System.out.println("Even: " + even + " " + "Odd: " + odd);
+    }
+}
