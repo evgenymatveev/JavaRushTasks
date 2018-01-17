b69eb50693ce696f7c71a89150f3bccba70b5048
evgenymatveev <evgeni2007@yandex.ru>
2018-01-17 23:51:20 +0300

    level 6 task 22

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0622/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task06.task0622;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Числа по возрастанию
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int arr[] = new int[5];
+        int n = 0;
+        while (n < 5) {
+            int x = Integer.parseInt(reader.readLine());
+            arr[n] = x;
+            n++;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            int min = arr[i];
+            int min_i = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < min) {
+                    min = arr[j];
+                    min_i = j;
+                }
+            }
+
+            if (i != min_i) {
+                int tmp = arr[i];
+                arr[i] = arr[min_i];
+                arr[min_i] = tmp;
+            }
+        }
+        for (int a : arr) {
+            System.out.println(a);
+        }
+    }
+}
