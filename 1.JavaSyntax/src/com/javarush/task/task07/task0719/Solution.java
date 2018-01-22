572ac63775a2cde9852094c3de13bac2ff0b9b45
evgenymatveev <evgeni2007@yandex.ru>
2018-01-22 22:17:29 +0300

    level 7 task 19

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0719/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task07.task0719;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Вывести числа в обратном порядке
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> list = new ArrayList<>();
+        int n = 0;
+        while (n < 10) {
+            list.add(Integer.parseInt(reader.readLine()));
+            n++;
+        }
+        for (int i = list.size() - 1; i >= 0; i--) {
+            System.out.println(list.get(i));
+        }
+
+    }
+}
