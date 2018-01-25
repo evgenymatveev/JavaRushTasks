517ed7c6842a02853ddcfff0c560c9ff4f9b6080
evgenymatveev <evgeni2007@yandex.ru>
2018-01-25 23:19:55 +0300

    level 7 task 28

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0728/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task07.task0728;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+В убывающем порядке
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[20];
+        for (int i = 0; i < 20; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+
+        sort(array);
+
+        for (int x : array) {
+            System.out.println(x);
+        }
+    }
+
+    public static void sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int max = array[i];
+            int max_index = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] > max) {
+                    max = array[j];
+                    max_index = j;
+                }
+            }
+            if (i != max_index) {
+                int tmp = array[i];
+                array[i] = array[max_index];
+                array[max_index] = tmp;
+            }
+        }
+    }
+}
