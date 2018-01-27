920f16e11e516c6732981eaca6ed48b05d5fd506
evgenymatveev <evgeni2007@yandex.ru>
2018-01-27 11:11:05 +0300

    level 8 task 13

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0813/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task08.task0813;
+
+import java.util.Set;
+import java.util.HashSet;
+
+/* 
+20 слов на букву «Л»
+*/
+
+public class Solution {
+    public static Set<String> createSet() {
+        HashSet<String> set = new HashSet<>();
+        set.add("Лапа");
+        set.add("Лампа");
+        set.add("Липа");
+        set.add("Лупа");
+        set.add("Лужа");
+        set.add("Лейка");
+        set.add("Лайка");
+        set.add("Лемон");
+        set.add("Линейка");
+        set.add("Лужайка");
+        set.add("Лист");
+        set.add("Листок");
+        set.add("Лужок");
+        set.add("Лама");
+        set.add("Липкий");
+        set.add("Любовь");
+        set.add("Лишний");
+        set.add("Лимончелло");
+        set.add("Лондон");
+        set.add("Лион");
+        return set;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
