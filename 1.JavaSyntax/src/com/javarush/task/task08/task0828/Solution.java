218cadb3348338dc519b702b80cf2949e7724d95
evgenymatveev <evgeni2007@yandex.ru>
2018-01-29 21:21:04 +0300

    level 8 task 28

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0828/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task08.task0828;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+/* 
+Номер месяца
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("January", 1);
+        map.put("February", 2);
+        map.put("March", 3);
+        map.put("April", 4);
+        map.put("May", 5);
+        map.put("June", 6);
+        map.put("July", 7);
+        map.put("August", 8);
+        map.put("September", 9);
+        map.put("October", 10);
+        map.put("November", 11);
+        map.put("December", 12);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String mouth = reader.readLine();
+        for (HashMap.Entry<String, Integer> v : map.entrySet()) {
+            String k = v.getKey();
+            if (mouth.equals(k)) {
+                System.out.println(v.getKey() + " is " + map.get(k) + " month");
+            }
+        }
+    }
+}
